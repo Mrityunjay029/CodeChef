9e352fe95d3d8ada92f24171f4fcf9de3bed51b7
Mrityunjay029 <77934726+Mrityunjay029@users.noreply.github.com>
2022-03-31 18:53:28 +0530

    Add files via upload

---
+++ CodeChef/Black_Cells_In_a_chessboard.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Black_Cells_In_a_chessboard {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * n / 2);
+    }
+}
