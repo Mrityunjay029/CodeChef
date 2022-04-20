1fb9a4b3658f1749a8be9664241ded19157c856e
Mrityunjay029 <77934726+Mrityunjay029@users.noreply.github.com>
2022-04-20 09:37:19 +0530

    Create ATM_MACHINE.java

---
+++ CodeChef/ATM_MACHINE.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    int arr[] = new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i] = sc.nextInt();
+		        if(k-arr[i] >= 0){
+		            k = k - arr[i];
+		            System.out.print("1");
+		        }
+		        else
+		        System.out.print("0");
+		    }
+		    System.out.println("");
+		}
+	}
+}
