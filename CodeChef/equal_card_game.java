8b644ff2de620224a0e44f5aaf82b1d84de2e9b4
Mrityunjay029 <77934726+Mrityunjay029@users.noreply.github.com>
2022-05-07 11:39:07 +0530

    Create equal_card_game.java

---
+++ CodeChef/equal_card_game.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int k = sc.nextInt();
+		    System.out.println(52%k);
+		}
+	}
+}
