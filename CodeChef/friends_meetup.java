cc0a3156c20f9440e49725054df45d86c9812d18
Mrityunjay029 <77934726+Mrityunjay029@users.noreply.github.com>
2022-05-09 10:51:52 +0530

    Create friends_meetup.java

---
+++ CodeChef/friends_meetup.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    if(x==y)
+		    System.out.println("YES");
+		    else if(x<y)
+		    System.out.println("NO");
+		    else if(x>y)
+		    System.out.println("YES");
+		}
+	}
+}
